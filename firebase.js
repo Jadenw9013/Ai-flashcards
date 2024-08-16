b25566f583798d411ba34f56e2c5f25351815869
Jadenw9013 <130799431+Jadenw9013@users.noreply.github.com>
2024-08-15 23:31:59 -0700

    changed keys

--- firebase.js
+++ firebase.js
@@ -1,24 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore}  from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration using environment variables
 const firebaseConfig = {
-  apiKey: "AIzaSyAz619yPhJJeKCJ7zThKQNM-l7G7ZcqEEw",
-  authDomain: "flashcardsaas-299a9.firebaseapp.com",
-  projectId: "flashcardsaas-299a9",
-  storageBucket: "flashcardsaas-299a9.appspot.com",
-  messagingSenderId: "324817914588",
-  appId: "1:324817914588:web:71f666baa454a9a4020219",
-  measurementId: "G-6LHSS48D19"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app)
+const db = getFirestore(app);
 
-export { db }
+export { db };
